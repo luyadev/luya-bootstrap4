a5bd28f4bcc0fb108fcdaa7745821f483fe816f7
Basil Suter <git@nadar.io>
2016-08-02 15:15:32 +0200

    added luya module to load blocks

---
+++ Module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace bootstrap4;
+
+/**
+ * Bootstrap4 Module
+ * 
+ * When adding this module to your configuration the bootstrap4 block will be added to your
+ * cmsadministration by running the `import` command.
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ */
+class Module extends \luya\base\Module
+{
+    
+}
