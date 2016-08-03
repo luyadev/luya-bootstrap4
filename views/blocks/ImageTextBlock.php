110355886116702f56dcbc7a4437a6a1703a762b
dev7ch <silvan@dev7.ch>
2016-08-03 13:44:02 +0200

    rewrite view from twig to php

--- views/blocks/ImageTextBlock.php
+++ views/blocks/ImageTextBlock.php
@@ -1,9 +1,28 @@
-<?
-
-// $vars
-// $cfgs
-// $extras
-
-?>
-
-Huhu!
+<? if ($vars['imagePosition'] == 'left'): ?>
+    <div class="image-text__wrapper">
+        <div class="col-sm-6 p-a-0">
+            <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
+        </div>
+        <div class="col-sm-6 p-a-0">
+            <?= $extras['text'] ?>
+        </div>
+    </div>
+<? elseif ($vars['imagePosition'] == 'right'): ?>
+    <div class="image-text__wrapper">
+        <div class="col-sm-6 p-a-0">
+            <?= $extras['text'] ?>
+        </div>
+        <div class="col-sm-6 p-a-0">
+            <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid <?= $vars['imageShapes'] ?> ?>"/>
+        </div>
+    </div>
+<? elseif ($vars['imagePosition'] == 'centered'): ?>
+    <div class="image-text__wrapper">
+        <div class="col-sm-12 p-a-0 text-xs-center">
+            <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid m-x-auto d-block <?= $vars['imageShapes'] ?> ?>"/>
+        </div>
+        <div class="col-sm-12 p-a-0">
+            <?= $extras['text'] ?>
+        </div>
+    </div>
+<? endif; ?>
