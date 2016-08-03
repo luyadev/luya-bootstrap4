9d3f0dd8373bf3abcc97b86db038bc32ae29c740
dev7ch <silvan@dev7.ch>
2016-08-03 13:43:12 +0200

    translations for ImageTextBlock

--- messages/de/bootstrap4.php
+++ messages/de/bootstrap4.php
@@ -9,4 +9,18 @@ return [
     'block_file_list.show_type_icon' => 'Dateityp-Icon anzeigen',
     'block_file_list.show_file_size' => 'Dateigrösse anzeigen',
     'block_image_text.block_name' => 'Bild mit Text',
+    'block_image_text.image_position' => 'Bildposition',
+    'block_image_text.left' => 'Links',
+    'block_image_text.right' => 'Rechts',
+    'block_image_text.center' => 'Zentriert',
+    'block_image_text.image_shape' => 'Bildform',
+    'block_image_text.no_shape' => 'Keine Form',
+    'block_image_text.rounded_corners' => 'Abgerundete Ecken',
+    'block_image_text.thumbnail' => 'Thumbnail mit Umrandung',
+    'block_image_text.circle' => 'Kreis',
+    'block_image_text.image' => 'Bild',
+    'block_image_text.help' => 'Bilder mit Text positionieren',
+    'block_image_text.no_content' => 'Es wurden noch keine Elemente hinzugefügt.',
+
+
 ];
