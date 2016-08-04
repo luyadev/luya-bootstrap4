5341b67214dfa4b764062c581eafcf3106e015da
Silvan <silvan@dev7.ch>
2016-08-04 11:10:27 +0200

    Update ImageTextBlock.php

--- views/blocks/ImageTextBlock.php
+++ views/blocks/ImageTextBlock.php
@@ -1,30 +1,32 @@
-<div class="container p-a-0">
-    <? if ($vars['imagePosition'] == 'left'): ?>
-        <div class="image-text__wrapper">
-            <div class="col-sm-6 p-a-0">
-                <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
+<? if (isset($vars['imagePosition'])): ?>
+    <div class="container p-a-0">
+        <? if ($vars['imagePosition'] == 'left'): ?>
+            <div class="image-text__wrapper">
+                <div class="col-sm-6 p-a-0">
+                    <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
+                </div>
+                <div class="col-sm-6 p-a-0">
+                    <?= $extras['text'] ?>
+                </div>
             </div>
-            <div class="col-sm-6 p-a-0">
-                <?= $extras['text'] ?>
+        <? elseif ($vars['imagePosition'] == 'right'): ?>
+            <div class="image-text__wrapper">
+                <div class="col-sm-6 p-a-0">
+                    <?= $extras['text'] ?>
+                </div>
+                <div class="col-sm-6 p-a-0">
+                    <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid <?= $vars['imageShapes'] ?> ?>"/>
+                </div>
             </div>
-        </div>
-    <? elseif ($vars['imagePosition'] == 'right'): ?>
-        <div class="image-text__wrapper">
-            <div class="col-sm-6 p-a-0">
-                <?= $extras['text'] ?>
+        <? elseif ($vars['imagePosition'] == 'centered'): ?>
+            <div class="image-text__wrapper">
+                <div class="col-sm-12 p-a-0 text-xs-center">
+                    <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid m-x-auto d-block <?= $vars['imageShapes'] ?> ?>"/>
+                </div>
+                <div class="col-sm-12 p-a-0">
+                    <?= $extras['text'] ?>
+                </div>
             </div>
-            <div class="col-sm-6 p-a-0">
-                <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid <?= $vars['imageShapes'] ?> ?>"/>
-            </div>
-        </div>
-    <? elseif ($vars['imagePosition'] == 'centered'): ?>
-        <div class="image-text__wrapper">
-            <div class="col-sm-12 p-a-0 text-xs-center">
-                <img src="<?= $extras['imageId']['source'] ?>" class="img-fluid m-x-auto d-block <?= $vars['imageShapes'] ?> ?>"/>
-            </div>
-            <div class="col-sm-12 p-a-0">
-                <?= $extras['text'] ?>
-            </div>
-        </div>
-    <? endif; ?>
-</div>
+        <? endif; ?>
+    </div>
+<? endif; ?>
