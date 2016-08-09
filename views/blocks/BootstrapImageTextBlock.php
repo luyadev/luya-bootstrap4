86b11bb09a9f7f5c614a2353f9681f9807982c9b
dev7ch <silvan@dev7.ch>
2016-08-09 10:51:06 +0200

    fix padding and clearfix from floated image block

--- views/blocks/BootstrapImageTextBlock.php
+++ views/blocks/BootstrapImageTextBlock.php
@@ -1,12 +1,12 @@
 <? if (isset($vars['imagePosition'])): ?>
-    <div class="container p-a-0 image-text">
+    <div class="clearfix image-text">
         <? if ($vars['imagePosition'] == 'left'): ?>
             <div class="image-text__wrapper">
                 <div class="col-sm-12 p-a-0">
                     <div class="image-text__wrapper__image hidden-xs-down" style="float:left;<? if (isset($cfgs['imageWidth'])):?>width: <?= $cfgs['imageWidth']?>; <? endif ?> max-width:100%;">
-                        <img src="<?= $extras['imageId']['source'] ?>" style=" padding-right:10px;" alt="<?= $vars['imageCaption'] ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
+                        <img src="<?= $extras['imageId']['source'] ?>" style="padding-right:10px;" alt="<?= $vars['imageCaption'] ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
                         <? if (isset($vars['imageCaption']) && (!empty($vars['imageCaption']))): ?>
-                            <span class="image-text__wrapper__image__caption <? if (isset($cfgs['imageCaptionVisibility']) && ($cfgs['imageCaptionVisibility'] == 0)): ?>  hidden-xs-up <? endif ?>">
+                            <span class="image-text__wrapper__image__caption <? if (isset($cfgs['imageCaptionVisibility']) && ($cfgs['imageCaptionVisibility'] == 0)): ?>  hidden-xs-up <? endif ?>" style="padding-right:10px;">
                                 <?= $vars['imageCaption'] ?>
                             </span>
                         <? endif ?>
@@ -28,7 +28,7 @@
                     <div class="image-text__wrapper__image hidden-xs-down" style="float:right;<? if (isset($cfgs['imageWidth'])):?>width: <?= $cfgs['imageWidth']?>; <? endif ?> max-width:100%;">
                         <img src="<?= $extras['imageId']['source'] ?>" style="padding-left: 10px;" alt="<? if (isset($vars['imageCaption']) && (!empty($vars['imageCaption']))): ?> <?= $vars['imageCaption'] ?> <? endif ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
                         <? if (isset($vars['imageCaption']) && (!empty($vars['imageCaption']))): ?>
-                            <span class="image-text__wrapper__image__caption">
+                            <span class="image-text__wrapper__image__caption <? if (isset($cfgs['imageCaptionVisibility']) && ($cfgs['imageCaptionVisibility'] == 0)): ?>  hidden-xs-up <? endif ?>" style="padding-left:10px;" >
                                 <?= $vars['imageCaption'] ?>
                             </span>
                         <? endif ?>
