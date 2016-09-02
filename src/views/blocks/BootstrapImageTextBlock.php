b94b0bfcc5e44350af0ba220a9e4256b0cdd5632
nadar <git@nadar.io>
2016-09-02 14:46:50 +0200

    isset block fix

--- src/views/blocks/BootstrapImageTextBlock.php
+++ src/views/blocks/BootstrapImageTextBlock.php
@@ -4,7 +4,7 @@
             <div class="image-text__wrapper">
                 <div class="col-sm-12 p-a-0">
                     <div class="image-text__wrapper__image hidden-xs-down" style="float:left;<?php if (isset($cfgs['imageWidth'])):?>width: <?= $cfgs['imageWidth']?>; <?php endif ?> max-width:100%;">
-                        <img src="<?= $extras['imageId']['source'] ?>" style="padding-right:10px;" alt="<?= $vars['imageCaption'] ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
+                        <img src="<?= $extras['imageId']['source'] ?>" style="padding-right:10px;" alt="<? if (isset($vars['imageCaption'])): echo $vars['imageCaption']; endif; ?>" class="img-fluid <?= $vars['imageShapes'] ?>"/>
                         <?php if (isset($vars['imageCaption']) && (!empty($vars['imageCaption']))): ?>
                             <div class="image-text__wrapper__image__caption <?php if (isset($cfgs['imageCaptionVisibility']) && ($cfgs['imageCaptionVisibility'] == 0)): ?>  hidden-xs-up <?php endif ?>" style="padding-right:10px;">
                                 <?= $vars['imageCaption'] ?>
