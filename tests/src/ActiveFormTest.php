74f1f84afb1dd49dc26decc2ac5e59171ff67264
Basil Suter <git@nadar.io>
2017-09-09 20:03:23 +0200

    use luya test suite

--- tests/src/ActiveFormTest.php
+++ tests/src/ActiveFormTest.php
@@ -3,32 +3,21 @@
 namespace luya\bootstrap4\tests\src;
 
 use Yii;
-use luya\bootstrap4\tests\Bootstrap4TestCase;
-use luya\bootstrap4\ActiveForm;
 use yii\base\Model;
+use luya\bootstrap4\ActiveForm;
+use luya\testsuite\cases\WebApplicationTestCase;
 
-class StubModel extends Model
+class Bootstrap4Test extends WebApplicationTestCase
 {
-    public $firstname = null;
-    
-    public function rules()
-    {
-        return [
-            [['firstname'], 'string'],
-        ];
-    }
-}
-
-class ActiveFromTest extends Bootstrap4TestCase
-{
-    private function trimmer($text)
-    {
-        $text = trim(preg_replace('/\s+/', ' ', $text));
-        
-        return str_replace(['> ', ' <'], ['>', '<'], $text);
-    }
-    
-    public function testLayout()
+	public function getConfigArray()
+	{
+		return [
+			'id' => 'bs4app',
+			'basePath' => dirname(__DIR__) . '/../',
+		];
+	}
+	
+    public function testActiveFormLayout()
     {
         Yii::setAlias('@webroot', dirname(__DIR__));
         $model = new StubModel();
@@ -39,23 +28,27 @@ class ActiveFromTest extends Bootstrap4TestCase
         echo $form->field($model, 'firstname');
         ActiveForm::end();
         
-        $this->assertContains('<div class="form-group row field-stubmodel-firstname"><label class="col-sm-2 col-form-label" for="stubmodel-firstname">Firstname</label><div class="col-sm-10"><input type="text" id="stubmodel-firstname" class="form-control" name="StubModel[firstname]"><div class="text-muted text-help"></div></div></div>', $this->trimmer(ob_get_clean()));
+        $this->assertContainsTrimmed('
+        	<div class="form-group row field-stubmodel-firstname">
+        		<label class="col-sm-2 col-form-label" for="stubmodel-firstname">Firstname</label>
+        		<div class="col-sm-10">
+        			<input type="text" id="stubmodel-firstname" class="form-control" name="StubModel[firstname]">
+        			<div class="form-control-feedback"></div>
+        		</div>
+        	</div>', ob_get_clean());
     }
 }
 
-/*
-<div class="form-group row field-dynamicmodel-wunsch">
-    <label class="col-sm-2 col-form-label" for="dynamicmodel-wunsch">Wunsch</label>
-    <div class="col-sm-10">
-        <textarea id="dynamicmodel-wunsch" name="DynamicModel[wunsch]"></textarea>
-    </div>
-    <div class="text-muted text-help"></div>
-</div>
- *
-<div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-      <div class="col-sm-10">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-      </div>
-    </div>
- */
+/* the inline stub model to use for the active form */
+
+class StubModel extends Model
+{
+	public $firstname = null;
+
+	public function rules()
+	{
+		return [
+				[['firstname'], 'string'],
+		];
+	}
+}
