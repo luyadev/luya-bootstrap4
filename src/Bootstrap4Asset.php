3abd6de7cea6c54aae0e9bcda1333c8c26f919b1
Basil <git@nadar.io>
2018-01-28 11:38:07 +0100

    Stable bootstrap 4 resources

--- src/Bootstrap4Asset.php
+++ src/Bootstrap4Asset.php
@@ -11,11 +11,11 @@ class Bootstrap4Asset extends \yii\web\AssetBundle
 {
     public $js = [
         '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-        '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js',
+        '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
     ];
     
     public $css = [
-        '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css',
+        '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
     ];
     
     public $depends = [
