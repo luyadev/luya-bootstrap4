6d07f46d9ef73ec080c1ef0954d4bc3bb4b84ea3
Basil Suter <git@nadar.io>
2018-01-30 18:10:51 +0100

    add phpdoc

--- src/BaseBootstrap4Block.php
+++ src/BaseBootstrap4Block.php
@@ -5,9 +5,12 @@ namespace luya\bootstrap4;
 use luya\cms\base\PhpBlock;
 
 /**
- * Base block for all generic blocks.
+ * Base block for all bootstrap4 blocks.
+ *
+ * The BaseBootstrap4Block helpts to allocate the view files for the blocks without using an alias.
  *
  * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
  */
 abstract class BaseBootstrap4Block extends PhpBlock
 {
