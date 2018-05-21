3d8765d8c4f4f698923a19673254c865126c5ca5
Basil Suter <git@nadar.io>
2018-05-21 21:54:36 +0200

    fix asset bug closes #10

--- src/Bootstrap4Asset.php
+++ src/Bootstrap4Asset.php
@@ -11,12 +11,12 @@ namespace luya\bootstrap4;
 class Bootstrap4Asset extends \yii\web\AssetBundle
 {
     public $js = [
-        ['//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js', ['integrity' => 'sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ', 'crossorigin' => 'anonymous']],
-        ['//stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js', ['integrity' => 'sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm', 'crossorigin' => 'anonymous']],  '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+        ['//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js', 'integrity' => 'sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ', 'crossorigin' => 'anonymous'],
+        ['//stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js', 'integrity' => 'sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm', 'crossorigin' => 'anonymous'],
     ];
     
     public $css = [
-        ['//stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css', ['integrity' => 'sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4', 'crossorigin' => 'anonymous']], 
+        ['//stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css', 'integrity' => 'sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4', 'crossorigin' => 'anonymous'], 
     ];
         
     public $depends = [
