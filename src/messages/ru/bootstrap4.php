bdbda9d6acc164e046364c255082f689172583e0
Robert Kuznetsov <Robot72@users.noreply.github.com>
2018-11-03 12:25:36 +0300

    Create bootstrap4.php for russian language (#20)
    * Create bootstrap4.php for russian language
    
    Add russian localization.
    
    * Update CHANGELOG.md
    
    Add russian translations.

---
+++ src/messages/ru/bootstrap4.php
@@ -0,0 +1,79 @@
+<?php
+
+// RU
+
+return [
+    'block_file_list.block_name' => 'Cписок файлов',
+    'block_file_list.files_label' => 'Файлы',
+    'block_file_list.show_type_label' => 'Показать тип файла',
+    'block_file_list.show_type_icon' => 'Показать иконку типа файла',
+    'block_file_list.show_file_size' => 'Показать размер файла',
+
+    'block_image.block_name' => 'Изображение',
+    'block_image.image' => 'Изображение',
+    'block_image.align' => 'Положение изображения',
+    'block_image.align_left' => 'Слева',
+    'block_image.align_right' => 'Справа',
+    'block_image.align_center' => 'По центру',
+    'block_image.show_caption' => 'Показывать заголовок',
+
+    'block_layout.block_name' => 'Макет: строка',
+    'block_layout.content' => 'Контент в строке',
+
+    'block_layout_column.block_name' => 'Макет: колонка',
+    'block_layout_column.column_count' => 'Размер XS up',
+    'block_layout_column.column_count_sm' => 'Размер SM up',
+    'block_layout_column.column_count_md' => 'Размер MD up',
+    'block_layout_column.column_count_lg' => 'Размер LG up',
+    'block_layout_column.column_count_xl' => 'Размер XL up',
+    'block_layout_column.content' => 'Контент в колонке',
+
+    'block_carousel.items' => 'Слайды',
+    'block_carousel.block_name' => 'Карусель',
+    'block_carousel.title' => 'Заголовок',
+    'block_carousel.caption' => 'Показывать заголовок',
+    'block_carousel.image' => 'Изображение',
+    'block_carousel.image_link' => 'Ссылка',
+
+    'block_image_text.block_name' => 'Изображение с текстом',
+    'block_image_text.image_position' => 'Позиция изображения',
+    'block_image_text.left' => 'Слева',
+    'block_image_text.right' => 'Справа',
+    'block_image_text.center' => 'По центру',
+    'block_image_text.image_shape' => 'Форма изображения',
+    'block_image_text.no_shape' => 'Нет формы',
+    'block_image_text.rounded_corners' => 'Округленные углы',
+    'block_image_text.thumbnail' => 'Миниатюра с рамкой',
+    'block_image_text.circle' => 'Круг',
+    'block_image_text.image' => 'Изображение',
+    'block_image_text.image.width' => 'Ширина изображения',
+    'block_image_text.image.caption' => 'Заголовок изображения',
+    'block_image_text.image.caption.visibility' => 'Отобразить заголовок изображения',
+    'block_image_text.help' => 'Добавление изображений и текста в динамическую обертку макета',
+    'block_image_text.no_text' => 'Текст еще не добавлен.',
+    'block_image_text.no_image' => 'Изображение еще не добавлено.',
+    'block_image_text.link' => 'Ссылка на изображение',
+    'block_image_text.file' => 'Прикрепить файл',
+    'block_image_text.file_download' => 'Загрузить файл через изображение',
+
+    'block_inlinetext.name'=> 'Встроенные текстовый блок',
+    'block_inlinetext.empty'=> 'Текст еще не был добавлен.',
+    'block_inlinetext.size-sm'=> 'Размер для мобильных устройств',
+    'block_inlinetext.size-md'=> 'Размер для планшетов',
+    'block_inlinetext.size-lg'=> 'Размер для настольных компьютеров',
+    'block_inlinetext.size-full'=> 'Полная ширина',
+    'block_inlinetext.size'=> 'ширина',
+
+    'block_inline_wrapper.label'=>'Разместить здесь встроенные блоки ...',
+    'block_inline_wrapper.name'=>'Контейнер встроенного блока',
+
+    'block_carousel.config_controls' => 'Элементы управления',
+    'block_carousel.config_indicators' => 'Индикатор',
+    'block_carousel.config_crossfade' => 'Перекрестное затухание',
+    'block_carousel.config_interval' => 'Интервал',
+    'block_carousel.config_keyboard' => 'Клавиатура',
+    'block_carousel.config_pause' => 'Пауза',
+    'block_carousel.config_ride' => 'Режим пролистывания',
+    'block_carousel.config_wrap' => 'Обертка',
+    'block_carousel.config_row' => 'Строка'
+];
