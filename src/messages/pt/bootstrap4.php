fdf323703a474b372d907baac11285f229417fdb
Andre Gomes <andre.gomes@amopromo.com>
2019-10-01 13:40:29 -0300

    bootstrap4-message-pt-translation

---
+++ src/messages/pt/bootstrap4.php
@@ -0,0 +1,81 @@
+<?php
+
+// EN
+
+return [
+    'block_file_list.block_name' => 'Lista de arquivos',
+    'block_file_list.files_label' => 'Arquivos',
+    'block_file_list.show_type_label' => 'Mostrar tipo do arquivo',
+    'block_file_list.show_type_icon' => 'Mostrar icone do tipo do arquivo',
+    'block_file_list.show_file_size' => 'Mostrar tamanho do arquivo',
+
+    'block_image.block_name' => 'Imagem',
+    'block_image.image' => 'Imagem',
+    'block_image.align' => 'Posição da Imagem',
+    'block_image.align_left' => 'Esquerda',
+    'block_image.align_right' => 'Direita',
+    'block_image.align_center' => 'Centro',
+    'block_image.show_caption' => 'Mostrar legenda',
+
+    'block_layout.block_name' => 'Layout: Linhas',
+    'block_layout.content' => 'Conteúdo da linha',
+
+    'block_layout_column.block_name' => 'Layout: Coluna',
+    'block_layout_column.column_count' => 'Tamanho XS acima',
+    'block_layout_column.column_count_sm' => 'Tamanho SM acima',
+    'block_layout_column.column_count_md' => 'Tamanho MD acima',
+    'block_layout_column.column_count_lg' => 'Tamanho LG acima',
+    'block_layout_column.column_count_xl' => 'Tamanho XL acima',
+    'block_layout_column.content' => 'Conteúdo da coluna',
+
+    'block_carousel.items' => 'Slides', //not translated
+    'block_carousel.block_name' => 'Carrossel',
+    'block_carousel.title' => 'Título',
+    'block_carousel.caption' => 'Legenda da imagem',
+    'block_carousel.image' => 'Imagem',
+    'block_carousel.image_link' => 'Link',
+
+    'block_image_text.block_name' => 'Imagem com Texto',
+    'block_image_text.image_position' => 'Posição da Imagem',
+    'block_image_text.left' => 'Esquerda',
+    'block_image_text.right' => 'Direita',
+    'block_image_text.center' => 'Centro',
+    'block_image_text.image_shape' => 'Forma da Imagem',
+    'block_image_text.no_shape' => 'Sem forma',
+    'block_image_text.rounded_corners' => 'Bordas arredondadas',
+    'block_image_text.thumbnail' => 'Miniatura com borda',
+    'block_image_text.circle' => 'Círculo',
+    'block_image_text.image' => 'Imaem',
+    'block_image_text.image.width' => 'Largura da Imagem',
+    'block_image_text.image.caption' => 'Legenda da imagem',
+    'block_image_text.image.caption.visibility' => 'Mostrar legenda',
+    'block_image_text.help' => 'Adicionar Imagens e texto em um layout wrapper dinâmico',
+    'block_image_text.no_text' => 'Nenhum texto foi adicionado.',
+    'block_image_text.no_image' => 'Nenhuma imagem foi adicionada.',
+    'block_image_text.link' => 'Link da imagem',
+    'block_image_text.file' => 'Anexar arquivo',
+    'block_image_text.file_download' => 'Download do arquivo pela imagem',
+
+    'block_inlinetext.name'=> 'Bloco de Texto em linha',
+    'block_inlinetext.empty'=> 'Nenhum texto foi adicionado.',
+    'block_inlinetext.size-sm'=> 'Tamanho mobile',
+    'block_inlinetext.size-md'=> 'Tamanho tablet',
+    'block_inlinetext.size-lg'=> 'Tamanho desktop',
+    'block_inlinetext.size-full'=> 'Largura completa',
+    'block_inlinetext.size'=> 'largura',
+
+    'block_inline_wrapper.label'=>'Coloque os blocos em linha aqui ...',
+    'block_inline_wrapper.name'=>'Conteiner de Bloco em Linha',
+
+    'block_carousel.config_block_css_class' => 'Classe CSS do Bloco',
+    'block_carousel.config_caption_css_class' => 'Classe CSS da Legenda',
+    'block_carousel.config_controls' => 'Controles',
+    'block_carousel.config_indicators' => 'Indicadores',
+    'block_carousel.config_crossfade' => 'Crossfade', //no translated
+    'block_carousel.config_interval' => 'Intervalo',
+    'block_carousel.config_keyboard' => 'Teclado',
+    'block_carousel.config_pause' => 'Pausa',
+    'block_carousel.config_ride' => 'Direção',
+    'block_carousel.config_wrap' => 'Wrap', //not translated
+    'block_carousel.config_row' => 'Linha'
+];
