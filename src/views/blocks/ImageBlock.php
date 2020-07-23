ddd875275b677f263779ffc9b36e25d36da6f230
Alex Schmid <schmid@netfant.ch>
2020-07-23 11:59:42 +0200

    Ajusted caption

--- src/views/blocks/ImageBlock.php
+++ src/views/blocks/ImageBlock.php
@@ -21,7 +21,11 @@ $align = $this->varValue('align', 'left');
     <div class="image">
         <figure class="figure d-block <?php if ($align === 'left'): ?>text-left<?php elseif ($align === 'right'): ?>text-right<?php else: ?>text-center<?php endif; ?>">
             <?php
-            $caption = !empty($image->caption) ? $image->caption : '';
+            if (!empty($image->caption)) {
+                $caption = $image->caption;
+            } else {
+                $caption = '';
+            }
             if ($this->cfgValue('lazyload', false)):
                 $options['src'] = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
                 if (!empty($caption)) {
