6efe7cf7926d509d3734ba8aefe419b46584ea7e
Wermeille Bastien <bastien.wermeille@gmail.com>
2021-10-05 07:52:33 +0200

    Add french translation (#35)

---
+++ src/messages/fr/bootstrap4.php
@@ -0,0 +1,83 @@
+<?php
+
+// FR
+
+return [
+    'block_file_list.block_name' => 'Liste des fichiers',
+    'block_file_list.files_label' => 'Fichiers',
+    'block_file_list.show_type_label' => 'Afficher le type des fichiers',
+    'block_file_list.show_type_icon' => 'Afficher l\'icon du type de fichier',
+    'block_file_list.show_file_size' => 'Afficher la taille du fichier',
+
+    'block_image.block_name' => 'Image',
+    'block_image.image' => 'Image',
+    'block_image.align' => 'Position de l\'image',
+    'block_image.align_left' => 'Gauche',
+    'block_image.align_right' => 'Droite',
+    'block_image.align_center' => 'Centre',
+    'block_image.show_caption' => 'Affficher la légende',
+    'block_image.lazyload' => 'Chargement différé',
+
+    'block_layout.block_name' => 'Disposition: Ligne',
+    'block_layout.content' => 'Contenu de la ligne',
+
+    'block_layout_column.block_name' => 'Disposition: Colonne',
+    'block_layout_column.column_count' => 'Taille XS et au dessus',
+    'block_layout_column.column_count_sm' => 'Taille SM et au dessus',
+    'block_layout_column.column_count_md' => 'Taille MD et au dessus',
+    'block_layout_column.column_count_lg' => 'Taille LG et au dessus',
+    'block_layout_column.column_count_xl' => 'Taille XL et au dessus',
+    'block_layout_column.content' => 'Contenu de la colonne',
+
+    'block_carousel.items' => 'Diapositives',
+    'block_carousel.block_name' => 'Carrousel',
+    'block_carousel.title' => 'Titre',
+    'block_carousel.caption' => 'Légende de l\'image',
+    'block_carousel.image' => 'Image',
+    'block_carousel.image_link' => 'Lien',
+
+    'block_image_text.block_name' => 'Image avec texte',
+    'block_image_text.image_position' => 'Position de l\'image',
+    'block_image_text.left' => 'Gauche',
+    'block_image_text.right' => 'Droite',
+    'block_image_text.center' => 'Centré',
+    'block_image_text.image_shape' => 'Forme de l\'image',
+    'block_image_text.no_shape' => 'Aucune forme',
+    'block_image_text.rounded_corners' => 'Coins arrondi',
+    'block_image_text.thumbnail' => 'Vignette avec bordure',
+    'block_image_text.circle' => 'Cercle',
+    'block_image_text.image' => 'Image',
+    'block_image_text.image.width' => 'Largeur de l\'image',
+    'block_image_text.image.caption' => 'Légende',
+    'block_image_text.image.caption.visibility' => 'Afficher la légende',
+    'block_image_text.help' => 'Ajouter des images et du texte dans un dispositif de mise en page dynamique ',
+    'block_image_text.no_text' => 'Aucun texte n\'a été ajouté pour le moment.',
+    'block_image_text.no_image' => 'Aucune image n\'a été ajoutée pour le moment.',
+    'block_image_text.link' => 'Lien de l\'image',
+    'block_image_text.file' => 'Pièce jointe',
+    'block_image_text.file_download' => 'Télécharger le fichier via l\'image',
+
+    'block_inlinetext.name'=> 'Bloc de texte en ligne',
+    'block_inlinetext.empty'=> 'Aucun texte n\'a été ajouté pour le moment.',
+    'block_inlinetext.size-sm'=> 'Taille mobile',
+    'block_inlinetext.size-md'=> 'Taille tablette',
+    'block_inlinetext.size-lg'=> 'Taille ordinateur',
+    'block_inlinetext.size-full'=> 'Plein écran',
+    'block_inlinetext.size'=> 'largeur',
+
+    'block_inline_wrapper.label'=>'Placez les blocs en ligne ici ...',
+    'block_inline_wrapper.name'=>'Conteneur de bloc en ligne',
+
+    'block_carousel.config_block_css_class' => 'Classe css de block',
+    'block_carousel.config_caption_css_class' => 'Classe CSS de la légende',
+    'block_carousel.config_controls' => 'Contrôles',
+    'block_carousel.config_indicators' => 'Indicateurs',
+    'block_carousel.config_crossfade' => 'Fondu',
+    'block_carousel.config_interval' => 'Interval',
+    'block_carousel.config_keyboard' => 'Clavier',
+    'block_carousel.config_pause' => 'Pause',
+    'block_carousel.config_ride' => 'Monter',
+    'block_carousel.config_wrap' => 'Envelopper',
+    'block_carousel.config_row' => 'Ligne',
+    'block_carousel.lazyload' => 'Chargement différé'
+];
