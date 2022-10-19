656aa0283483b3d8017433cd4be7dec3e63d8a31
Dennis <dennisgon@users.noreply.github.com>
2022-10-19 14:18:37 +0700

    add translation bahasa (#37)

---
+++ src/messages/id/bootstrap4.php
@@ -0,0 +1,83 @@
+<?php
+
+// ID
+
+return [
+    'block_file_list.block_name' => 'List File',
+    'block_file_list.files_label' => 'File',
+    'block_file_list.show_type_label' => 'Tampilkan tipe file',
+    'block_file_list.show_type_icon' => 'Tampilkan tipe ikon file',
+    'block_file_list.show_file_size' => 'Tampilkan besar file',
+
+    'block_image.block_name' => 'Gambar',
+    'block_image.image' => 'Gambar',
+    'block_image.align' => 'Posisi Gambar',
+    'block_image.align_left' => 'Kiri',
+    'block_image.align_right' => 'Kanan',
+    'block_image.align_center' => 'Tengah',
+    'block_image.show_caption' => 'Tampilkan caption',
+    'block_image.lazyload' => 'LazyLoad',
+
+    'block_layout.block_name' => 'Layout: Baris',
+    'block_layout.content' => 'konten baris',
+
+    'block_layout_column.block_name' => 'Layout: Kolom',
+    'block_layout_column.column_count' => 'Ukuran XS up',
+    'block_layout_column.column_count_sm' => 'Ukuran SM up',
+    'block_layout_column.column_count_md' => 'Ukuran MD up',
+    'block_layout_column.column_count_lg' => 'Ukuran LG up',
+    'block_layout_column.column_count_xl' => 'Ukuran XL up',
+    'block_layout_column.content' => 'Konten kolom',
+
+    'block_carousel.items' => 'Slide',
+    'block_carousel.block_name' => 'Carousel',
+    'block_carousel.title' => 'Judul',
+    'block_carousel.caption' => 'Camption gambar',
+    'block_carousel.image' => 'Gambar',
+    'block_carousel.image_link' => 'Tautan',
+
+    'block_image_text.block_name' => 'Gambar dengan teks',
+    'block_image_text.image_position' => 'posisi gambar',
+    'block_image_text.left' => 'Kiri',
+    'block_image_text.right' => 'Kanan',
+    'block_image_text.center' => 'Tengah',
+    'block_image_text.image_shape' => 'Bentuk gambar',
+    'block_image_text.no_shape' => 'Tidak ada bentuk',
+    'block_image_text.rounded_corners' => 'Lengkungan tepi',
+    'block_image_text.thumbnail' => 'Gambar kecil dengan batasan',
+    'block_image_text.circle' => 'Lingkaran',
+    'block_image_text.image' => 'Gambar',
+    'block_image_text.image.width' => 'Lebar Gambar',
+    'block_image_text.image.caption' => 'Caption Gambar',
+    'block_image_text.image.caption.visibility' => 'Tampilkan caption image',
+    'block_image_text.help' => 'Add Images and text into a dynamic layout wrapper',
+    'block_image_text.no_text' => 'Tidak ada teks yang ditambahkan.',
+    'block_image_text.no_image' => 'Tidak ada gambar yang ditambahkan.',
+    'block_image_text.link' => 'Tautan gambar',
+    'block_image_text.file' => 'Menempel file',
+    'block_image_text.file_download' => 'Unduh file melalui gambar',
+
+    'block_inlinetext.name'=> 'Inline Text Block',
+    'block_inlinetext.empty'=> 'Tidak ada teks yang ditambahkan.',
+    'block_inlinetext.size-sm'=> 'Ukuran mobile',
+    'block_inlinetext.size-md'=> 'Ukuran tablet',
+    'block_inlinetext.size-lg'=> 'Ukuran desktop',
+    'block_inlinetext.size-full'=> 'Lebar penuh',
+    'block_inlinetext.size'=> 'Lebar',
+
+    'block_inline_wrapper.label'=>'Taruh inline blocks disini ...',
+    'block_inline_wrapper.name'=>'Inline Block Kontainer',
+
+    'block_carousel.config_block_css_class' => 'Block CSS Class',
+    'block_carousel.config_caption_css_class' => 'Caption CSS Class',
+    'block_carousel.config_controls' => 'Kontrol',
+    'block_carousel.config_indicators' => 'Indikator',
+    'block_carousel.config_crossfade' => 'Crossfade',
+    'block_carousel.config_interval' => 'Selang',
+    'block_carousel.config_keyboard' => 'Keyboard',
+    'block_carousel.config_pause' => 'Hentikan',
+    'block_carousel.config_ride' => 'Tunggangi',
+    'block_carousel.config_wrap' => 'Pindahkan',
+    'block_carousel.config_row' => 'Baris',
+    'block_carousel.lazyload' => 'LazyLoad'
+];
